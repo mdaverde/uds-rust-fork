c1ea86e81a15ffe9f73130aeba212552172c423a
Milan <milanlandaverde@gmail.com>
2021-10-10 13:03:20 -0400

    moves abstract-server to main

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Working on a [custom Rust toolchain](https://github.com/mdaverde/rust/tree/uds-a
 As a test client to use against examples you can use [socat](https://www.redhat.com/sysadmin/getting-started-socat):
 
 ```sh
-$ echo "hello world" | socat ABSTRACT_CONNECT:namespace_goes_here -
+$ echo "hello world" | socat ABSTRACT-CONNECT:namespace_goes_here -
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+#![feature(unix_socket_abstract)]
+use std::os::{unix};
+use std::io::prelude::*;
+
 fn main() {
-    println!("Unix domain socket abstract examples in rust");
+    // TODO: move namespace config into param
+    let addr = unix::net::SocketAddr::from_abstract_namespace(b"hello").unwrap();
+    println!("Starting session on namespace: {:?}", addr);
+
+    let socket = unix::net::UnixListener::bind_addr(&addr).unwrap();
+    for (i, stream) in socket.incoming().enumerate() {
+        match stream {
+            Ok(mut stream) => {
+                // std::thread::spawn(|| handle_client(stream));
+                let mut received = String::new();
+                stream.read_to_string(&mut received).unwrap();
+                println!("[received ({})] {}", i, received);
+                // TODO: add ability to write from stdin
+                if let Ok(()) = stream.write_all(b"hello from server") {
+                    stream.flush().unwrap();
+                }
+            }
+            Err(_err) => {
+                println!("Not successful");
+            }
+        }
+    }
 }
